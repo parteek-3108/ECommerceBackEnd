d247fe0f443acded2844b2d06163572145aad1ab
parteek-3108 <71115539+parteek-3108@users.noreply.github.com>
2021-03-30 23:33:12 +0530

    Save to Database pending

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const bodyparser = require("body-parser");
 var fs = require("fs");
 var cors = require("cors");
 var app = express();
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
 app.use(cors());
 const port = 4000;
 app.get("/", function (req, res) {
-  console.log("Home Page");
+  // console.log("Home Page");
   res.send("Home page");
 });
 app.get("/getProducts", function (req, res) {
@@ -18,6 +21,11 @@ app.get("/getProducts", function (req, res) {
       res.send(JSON.parse(data).ProductData);
     }
   });
+});
+app.post("/setCart", function (req, res) {
+  // re.setHeader({ "Content-type": "application/json" });
+  console.log(req.body);
+  res.end("hi");
 });
 app.get("/getCart", function (req, res) {
   console.log("Trying to fetch");
